acc45c52cb0e361fcba1978cd9feff683c50733b
dev at <battuosai01@gmail.com>
2020-06-25 16:44:43 +0700

    Updates

---
+++ precache-manifest.313444a0682a0819a8ea1b680412e392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d90547060b79b080ce5a20de3d12f2b",
+    "url": "/navbar-menu-react/index.html"
+  },
+  {
+    "revision": "335ccccd70b274494421",
+    "url": "/navbar-menu-react/static/css/main.94ec2250.chunk.css"
+  },
+  {
+    "revision": "7c553e977ae9653331ca",
+    "url": "/navbar-menu-react/static/js/2.5c474107.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/navbar-menu-react/static/js/2.5c474107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335ccccd70b274494421",
+    "url": "/navbar-menu-react/static/js/main.6ed357b8.chunk.js"
+  },
+  {
+    "revision": "5ecac8283530a1681a07",
+    "url": "/navbar-menu-react/static/js/runtime-main.18514ca1.js"
+  }
+]);
